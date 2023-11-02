7dc7e2702f0d986c469860c9c3bd42b92f566974
Christopher Hounnou <126211266+chris-hounnou@users.noreply.github.com>
2023-11-02 08:46:20 -0500

    -HComponentDesign error is fixed

--- capstone/src/pages/Home.js
+++ capstone/src/pages/Home.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import styles from "./HComponentDesign.css";
+
 function Home () {
 
     return (
         <div className="Home">
             <h1 className='Title'> Welcome To The Land of Ice and Fire</h1>
+            
             <p> This is the home page to the Game of Thrones App
                 navigate to other characters and lands in the features below. 
             </p>
