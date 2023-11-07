83746ce1868a9268e3af5657dbe61e8f6c75ac7b
Christopher Hounnou <126211266+chris-hounnou@users.noreply.github.com>
2023-11-06 21:34:14 -0600

    -

--- capstone/src/pages/Home.js
+++ capstone/src/pages/Home.js
@@ -5,7 +5,7 @@ function Home () {
 
     return (
         <div className="Home">
-            <h1 className='Title'> Welcome To The Land of Ice and Fire</h1>
+            <h1 className='Title'> Welcome To The Land of Ice and Fire!</h1>
             
             <p> This is the home page to the Game of Thrones App
                 navigate to other characters and lands in the features below. 
