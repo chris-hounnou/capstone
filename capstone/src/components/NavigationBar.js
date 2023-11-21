ace3886392eac7f4f482411b437a90c701ca25db
Christopher Hounnou <126211266+chris-hounnou@users.noreply.github.com>
2023-11-20 19:47:51 -0600

    -commit

--- capstone/src/components/NavigationBar.js
+++ capstone/src/components/NavigationBar.js
@@ -2,28 +2,28 @@ import React from 'react'
 import { NavLink } from "react-router-dom";
 import styles from "./NavigationBar.module.css";
 
-function NavigationBar () {
+function NavigationBar() {
 
-    const routes = [
-        {to: "/", text: "Home"},
-        {to: "/Characters", text: "Characters"},
-        // {to: "/Character", text: "Character"},
-        {to: "/Houses", text: "Houses"}
-      ]; 
+  const routes = [
+    { to: "/", text: "Home" },
+    { to: "/Characters", text: "Characters" },
+    // {to: "/Character", text: "Character"},
+    { to: "/Houses", text: "Houses" }
+  ];
 
-    return (
-        <nav className={styles.navigationBar}>
-        {routes.map((route, index) => 
-          <NavLink className={styles.navLink}
-      key={index}
-      activeclassname={styles.active}
-      to={route.to}
-    >
-      {route.text}
-    </NavLink>
-          )}
-        </nav>
-      );
+  return (
+    <nav className={styles.navigationBar}>
+      {routes.map((route, index) =>
+        <NavLink className={styles.navLink}
+          key={index}
+          activeclassname={styles.active}
+          to={route.to}
+        >
+          {route.text}
+        </NavLink>
+      )}
+    </nav>
+  );
 
 }
 
